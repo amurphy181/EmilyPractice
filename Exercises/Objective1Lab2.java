20c90162d3e003fcf595baca6cef8f44cd0a9d6c
Alex Murphy <alexmurphy@Andrews-MBP-2.hsd1.co.comcast.net>
2019-05-04 20:48:13 -0600

    Objective1Labs2+3

---
+++ Exercises/Objective1Lab2.java
@@ -0,0 +1,6 @@
+public class Objective1Lab2{
+  public static void main(String[] args ){
+    System.out.println("Emily Lau");
+    System.out.println( "30 :( #tooold");
+  }
+}
---
+++ Exercises/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+  public static void main(String[] args ){
+    System.out.println("Emily Lau");
+    System.out.println(" 30 :(");
+    System.out.println("Massachusetts");
+  }
+}
